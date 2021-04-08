7dc3d7ee03365e6103c7c1faa6709795b3509103
kuy-ok-cho <okj9412@naver.com>
2021-04-08 14:54:20 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import React from "react";
 import "./App.css";
+// import Axios from "axios";
 
-function App() {
-  return <div className='App'>RealTime weather App</div>;
+class App extends React.Component {
+  componentDidMount() {
+    // console.log("render mounted");
+    if (navigator.geolocation) {
+      // console.log("supported");
+      navigator.geolocation.getCurrentPosition((position) => {
+        console.log(position.coords.latitude, position.coords.longitude);
+      });
+    } else {
+      // console.log("not supported");
+    }
+  }
+
+  render() {
+    return <div className='App'>RealTime weather App</div>;
+  }
 }
 
 export default App;
